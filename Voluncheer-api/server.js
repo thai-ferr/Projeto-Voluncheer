af0837f80756f1c6c8b617283fcf8168c1ec29c7
Samuel Alves <newyelken5248@gmail.com>
2026-04-06 10:49:44 -0300

    Add files via upload

---
+++ Voluncheer-api/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors' // Importe o cors
+import basicRoutes from './basicRoutes.js' 
+import userRoutes from './userRoutes.js'
+
+const app = express()
+
+app.use(cors()) // Libera o acesso para o seu front-end
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000 // Garante que rode na 3000 se não houver .env
+
+app.use(basicRoutes)
+app.use(userRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
